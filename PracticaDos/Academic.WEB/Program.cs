dd55015dc8c967be36b99185434b9ef0bcf384f9
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-09-28 17:00:06 -0500

    Cuarto Commit

--- PracticaDos/Academic.WEB/Program.cs
+++ PracticaDos/Academic.WEB/Program.cs
@@ -1,4 +1,5 @@
 using Academic.WEB;
+using Academic.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+await builder.Build().RunAsync();
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") });
+
+builder.Services.AddScoped<IRepository, Repository>();
+//builder.Services.AddSweetAlert2();
 await builder.Build().RunAsync();
