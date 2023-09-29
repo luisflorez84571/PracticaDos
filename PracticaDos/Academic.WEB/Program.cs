f822ae8ee5d314bb73fb44cb86c2a8d1b5576c60
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-09-28 21:01:55 -0500

    cuarto Commit

--- PracticaDos/Academic.WEB/Program.cs
+++ PracticaDos/Academic.WEB/Program.cs
@@ -1,7 +1,5 @@
 using Academic.WEB;
 using Academic.WEB.Repositories;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
